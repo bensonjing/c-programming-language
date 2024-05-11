31b68064d2096633376dfa0737ee5b3c75c77124
Benson Jing <bensonjing@icloud.com>
2024-05-11 11:34:19 -0700

    test: add test for alloc

--- linesorting/test/main.c
+++ linesorting/test/main.c
@@ -1,14 +1,39 @@
-#include <assert.h>
-#include "../src/memory.h" 
+#include <assert.h> 
+#include <stdio.h>
+#include "../include/memory.h" 
 
-extern char *alloc(int);
+void alloc_size_0(void);
+void alloc_size_4(void); 
+void alloc_size_10004(void);
 
-int main()
+int main() 
 {
-	char *first, *second; 
-	first = second = alloc(4); 
-	second = alloc(4);
-	assert(second-first != 4);
+	alloc_size_0(); 
+	alloc_size_4();
+	alloc_size_10004();
 
 	return 0;
+}
+
+void alloc_size_0(void)
+{
+	char *first = alloc(0); 
+	char *second = alloc(0); 
+	assert(second-first == 0);
+	printf("[PASS] alloc size 0\n");
+}
+
+void alloc_size_4(void)
+{
+	char *first = alloc(4); 
+	char *second = alloc(4); 
+	assert(second-first == 4);
+	printf("[PASS] alloc size 4\n");
+}
+
+void alloc_size_10004(void)
+{
+	char *first = alloc(10004); 
+	assert(first == 0);
+	printf("[PASS] alloc size 10004\n");
 }
